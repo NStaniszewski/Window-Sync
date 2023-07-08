d679ffdcb3d3dd8b18fe0dfe1dbf3bff51237e14
NStaniszewski <93354258+NStaniszewski@users.noreply.github.com>
2023-07-08 14:23:30 -0400

    Timer added
    added a timer for how long each key was pressed

--- sync_runner.py
+++ sync_runner.py
@@ -54,17 +54,6 @@ def auto_input(windows,script):
                 offset_time+=.31
         s.run()
 
-def input_sync(windows):
-    s=sched.scheduler(time.time,time.sleep)
-    offset_time=1
-    for window in windows:
-        press_keyboard(window,s,0x44,.1+offset_time,1)
-
-
-
-        offset_time+=.31
-    s.run()
-
 def press_keyboard(window,s:sched.scheduler,key,start_time,hold_time):
     prio=2
     foreground_time=.15
@@ -131,9 +120,40 @@ def scripty(windows):
     script=pull_script(file_name+".txt")
     auto_input(windows,script)
 
+def time_key_press(key):
+    global t
+    t=time.time()
+    
+def time_key_release(key):
+    global t
+    global time_held
+    global sync_list
+    time_held=round(time.time()-t,2)
+    if syncing:
+        if key!=Key.f9 and key!=Key.f10:
+            sync_list.append([key,time_held])
+    print(sync_list)
+
+def input_sync(windows):
+    vk_map={'w':0x57,'a':0x41,'s':0x53,'d':0x44,'p':0x55,Key.shift:0xA0,Key.ctrl_l: 0xA2,Key.space:0x20,Key.home:0x24,Key.end:0x23}
+    s=sched.scheduler(time.time,time.sleep)
+    offset_time=0
+    global key
+    global time_held
+    for window in windows:
+        runtime=0
+        for input in sync_list:
+            press_keyboard(window,s,vk_map[input[0]],offset_time+runtime,input[1])
+            runtime+=input[1]
+        offset_time+=.31
+    s.run()
+
 def main():
     scripting=False #change to True if doing scripts, False for synching
     global syncing;syncing=False
+    global sync_list;sync_list=[]
+    global t;t=time.time()
+    global key;key=''
     def on_f9(key):
         global syncing
         if key==Key.f9:
@@ -151,13 +171,18 @@ def main():
     window_list = boot_stuff()
     check_selected_windows(window_list)
     start_end.start()
+
     if scripting:
         scripty(window_list)
     else:
+        timer_start=Listener(on_press=time_key_press)
+        timer_end=Listener(on_release=time_key_release)
+        timer_start.start()
+        timer_end.start()
         while True:
-            if syncing:
+            if not syncing and sync_list!=[]:
                 input_sync(window_list)
-        
+                sync_list=[]
 
 if __name__ == "__main__":
     main()
